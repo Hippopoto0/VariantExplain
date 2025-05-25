6e1c8a0f847189d70096f3385a40fdff164b7484
Hippopoto0 <67485469+Hippopoto0@users.noreply.github.com>
2025-05-25 16:32:40 +0100

    streamlit succesffuly callign rag code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 import streamlit as st
 import requests
-from streamlit_chat import message
+from parse import VCFParser
+from rag import RAG
+import tempfile
+import json
 
 # Set page title
 st.title("Variant Explain App")
@@ -15,6 +19,7 @@ st.markdown("""
 Welcome to the Variant Explain App! This application helps you analyze and understand genetic variants.
 """)
 
+
 # # Add a simple input widget
 # with st.sidebar:
 #     st.header("Settings")
@@ -22,13 +27,32 @@ Welcome to the Variant Explain App! This application helps you analyze and under
 
 # Main content area
 
+variant_input: UploadedFile = st.file_uploader("Upload a VCF file", type="vcf")
+
 st.write("### Variant Analysis")
 if variant_input:
-    st.write(f"You entered: {variant_input}")
+    st.write(f"You uploaded: {variant_input}")
     st.write("This is where the analysis results will appear.")
 
-st.status("Variant Explain is running", state="running")
+state_title = "Generating output"
+if st.button("Run Variant Explain"):
+    with st.status(state_title):
+        st.write("Generating VEP annotations")
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.vcf') as temp_vcf:
+            temp_vcf.write(variant_input.read())
+            temp_vcf_path = temp_vcf.name
+        
+        parser = VCFParser(temp_vcf_path)
+        
+        st.write("Querying GWAS...")
+        
+        rag = RAG()
+        found_traits = rag.search_annotations(json.dumps(parser.annotation))
 
+        st.write("Generating abstracts...")
+        abstracts = rag.append_pubmed_abstracts(found_traits)
+
+        st.write("Done!")
 
 # Add a footer
 st.markdown("---")
